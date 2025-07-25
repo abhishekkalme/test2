2bd161281ee2d508cfdaa17c5251a07acf587373
Abhishek Kalme <abhishekkalme0@gmail.com>
2025-07-25 18:12:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,128 @@
-# test2
+<h1 align="center">Open-source-practice Pull Requests</h1>
+
+<div align="center">
+<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section --> 
+ 
+[![All Contributors](https://img.shields.io/badge/all_contributors-1-orange.svg?style=flat-square)](#contributors-)
+<a href="https://github.com/recodehive/recode-website/stargazers"><img src="https://img.shields.io/github/stars/recodehive/recode-website" alt="Stars Badge"/></a>
+<a href="https://github.com/recodehive/recode-website/network/members"><img src="https://img.shields.io/github/forks/recodehive/recode-website" alt="Forks Badge"/></a> 
+<a href="https://github.com/recodehive/recode-website/pulls"><img src="https://img.shields.io/github/issues-pr/recodehive/recode-website" alt="Pull Requests Badge"/></a>
+<a href="https://github.com/recodehive/recode-website/issues"><img src="https://img.shields.io/github/issues/recodehive/recode-website" alt="Issues Badge"/></a>
+<a href="https://github.com/recodehive/recode-website/graphs/contributors"><img alt="GitHub contributors" src="https://img.shields.io/github/contributors/recodehive/recode-website?color=2b9348"></a>
+<a href="https://github.com/recodehive/recode-website/LICENSE"><img src="https://img.shields.io/github/license/recodehive/recode-website?color=2b9348" alt="License Badge"/></a>
+[![](https://visitcount.itsvg.in/api?id=Opensource-practice&label=Profile%20Views&color=0&icon=5&pretty=true)](https://visitcount.itsvg.in)
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
+This is the all in one place for documentation help regarding How to contribute.
+</div>
+
+
+##
+
+Now, resolve your all doubts and communicate with our all contributors.
+
+[![](https://img.shields.io/badge/Discord-5865F2.svg?style=for-the-badge&logo=Discord&logoColor=white)](https://discord.gg/Yxv9RA3r) [![Follow on LinkedIn](https://img.shields.io/badge/Follow%20on-LinkedIn-blue?style=for-the-badge&logo=linkedin)](https://www.linkedin.com/in/sanjay-k-v/)
+
+
+## Getting Started
+
+To get started with contributing to Recode-Hive, please refer to our [Contributing Guidelines](CONTRIBUTING.md).
+
+Follow these steps:
+
+
+```mermaid
+flowchart LR
+    Fork[Fork the project]-->branch[Create a New Branch]
+    branch-->Edit[Edit file]
+    Edit-->commit[Commit the changes]
+    commit -->|Finally|creatpr((Create a Pull Request))
+```
+
+1. **Clone the repository:** 
+   ```bash
+   git clone https://github.com/your-username/recodehive-website.git
+   ```
+
+2. **Navigate to the project directory:**
+   ```bash
+   cd recodehive-website
+   ```
+
+3. **Install dependencies:**
+   ```bash
+   npm install
+   ```
+
+4. **Running the Application:**
+    There is a high chance you will face this issue due to the Next.js 18 version conflict, which is global , so type the below to fix it and ignore the warnings while setup.
+
+   ```bash
+   npm install --legacy-peer-deps
+   ```
+
+    Once you have installed the dependencies, you can run the application locally using:
+    ```bash
+    npm i
+    ```
+    ```bash
+    npm start
+    ```
+
+    This command will start a development server and open the application in your default web browser.
+
+**If you'd like to contribute to CodeHarborHub, please follow these guidelines:**
+
+- **Fork** the repository and clone it locally.
+- Create a new branch for your feature or bug fix: `git checkout -b feature-name`
+- Make your changes and test thoroughly.
+- Commit your changes: `git commit -m "Brief description of your changes"`
+- Push to the branch: `git push origin feature-name`
+- Submit a pull request detailing your changes.
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Badges
+
+![GitHub commit activity (yearly)](https://img.shields.io/github/commit-activity/y/RecodeHive/recode-website)
+![GitHub commit activity (monthly)](https://img.shields.io/github/commit-activity/m/RecodeHive/recode-website)
+![GitHub commit activity (weekly)](https://img.shields.io/github/commit-activity/w/RecodeHive/recode-website)
+![GitHub commit activity (daily)](https://img.shields.io/github/commit-activity/t/RecodeHive/recode-website)
+![GitHub Org's stars](https://img.shields.io/github/stars/RecodeHive/recode-website)
+![GitHub License](https://img.shields.io/github/license/RecodeHive/recode-website)
+![GitHub forks](https://img.shields.io/github/forks/RecodeHive/recode-website)
+![GitHub watchers](https://img.shields.io/github/watchers/RecodeHive/recode-website)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/RecodeHive/recode-website)
+![GitHub repo size](https://img.shields.io/github/repo-size/RecodeHive/recode-website)
+![GitHub file/directory count](https://img.shields.io/github/directory-file-count/RecodeHive/recode-website)
+![GitHub open issues](https://img.shields.io/github/issues/RecodeHive/recode-website)
+![GitHub closed issues](https://img.shields.io/github/issues-closed-raw/RecodeHive/recode-website)
+![GitHub open PRs](https://img.shields.io/github/issues-pr/RecodeHive/recode-website)
+![GitHub closed PRs](https://img.shields.io/github/issues-pr-closed/RecodeHive/recode-website)
+![Last commit](https://img.shields.io/github/last-commit/RecodeHive/recode-website)
+
+
+## Contributors
+
+<a href="https://github.com/RecodeHive/recode-website/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=RecodeHive/recode-website" />
+</a>
+
+
+<div align="center">
+
+Happy open-source contributions and here’s to your career success! 🎉
+<br>
+### recode-hive 2025
+
+[Website](https://recodehive.com/) | [Instagram](https://www.instagram.com/nomad_brains/) | [LinkedIn](https://www.linkedin.com/in/sanjay-k-v/) | [Twitter](https://x.com/sanjay_kv_) | [YouTube](https://www.youtube.com/@RecodeHive)<br>
+**🔔 Don’t miss a beat!** <br>
+  Subscribe to receive our newsletter directly in your inbox for the latest career insights & tailored to your journey.<br>
+[![Subscribe to Our Newsletter](https://img.shields.io/badge/Subscribe%20to%20Our%20Newsletter-%F0%9F%93%A9-blue)](https://recodehive.substack.com/) <br>
+
+</div>
+
+
+
---
+++ docusaurus.config.ts
@@ -0,0 +1,295 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "Recode Hive",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
+
+  url: "https://your-docusaurus-site.example.com",
+  baseUrl: "/",
+
+  organizationName: "facebook",
+  projectName: "docusaurus",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Google Analytics
+  scripts: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-W02Z2VJYCR',
+      async: true,
+    },
+    {
+      content: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-W02Z2VJYCR', {
+          debug_mode: ${process.env.NODE_ENV !== 'production' ? 'true' : 'false'}
+        });
+      `,
+    },
+  ],
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.ts"),
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Recode Hive",
+      logo: {
+        alt: "RecodeHive Logo",
+        src: "img/logo.png",
+      },
+      items: [
+        {
+          type: "dropdown",
+          html: '<span class="nav-emoji">📚</span> Docs',
+          position: "left",
+          items: [
+            {
+              type: "html",
+              value: `<div class="grid grid-cols-3 gap-4 w-xl">
+                <a class="border-r col-span-1" href="/courses/">Tutorials</a>
+                <div class="grid grid-cols-4 col-span-2">
+                  <a href="/docs/category/sql/" class="nav__icons"> <img src="/icons/sql.svg" title="SQL" alt="SQL" /> </a>
+                  <a href="/docs/category/python/" class="nav__icons"> <img src="/icons/python.svg" title="Python" alt="Python" /> </a>
+                  <a href="/docs/category/GitHub/" class="nav__icons" > <img src="/icons/github.svg" title="GitHub" alt="GitHub" /> </a>
+                  <a href="/docs/category/nextjs/" class="nav__icons" > <img src="/icons/nextjs.svg" title="Nextjs" alt="Nextjs" /> </a>
+                </div>
+              </div>`,
+            },
+            {
+              type: "html",
+              value: '<hr style="margin: 0.3rem 0;">',
+            },
+            {
+              type: "html",
+              value: `<div class="grid grid-cols-3 gap-4">
+                <a class="border-r col-span-1" href="/courses/"> Courses </a>
+                <div class="grid grid-cols-4 col-span-2">
+                  <a href="https://www.youtube.com/watch?v=GrTV59Y84S8&list=PLrLTYhoDFx-kiuFiGQqVpYYZ56pIhUW63&ab_channel=RecodeHive" class="nav__icons"> <img src="/icons/git.svg" alt="git" /> </a>
+                  <a href="https://www.youtube.com/watch?v=O1ahDsq8DU0&list=PLrLTYhoDFx-k62rLLajSB-jeqKwLkDrkF&ab_channel=RecodeHive" class="nav__icons"> <img src="/icons/postman.svg" alt="Postman" /> </a>
+                  <a href="/docs/category/google-student-ambassador" class="nav__icons"> <img src="https://avatars.githubusercontent.com/u/222021622?s=400&u=cb88492d19d9023cac470c3959b25285bb5abcfa&v=4" alt="Google" /> </a>
+                </div>
+              </div>`,
+            },
+            {
+              type: "html",
+              value: '<hr style="margin: 0.3rem 0;">',
+            },
+            {
+              type: "html",
+              value: `<div class="grid grid-cols-3 gap-4">
+                <a  class="border-r col-span-1" href="#" target="_self"> Interview Prep </a>
+                <div class="grid grid-cols-1 col-span-2">
+                  <a href="/docs/category/Technical/" target="_self" class="nav__icons"> 🧩Technical </a> <br />
+                  <a href="#" target="_self" class="nav__icons"> 💡Behaviour </a>
+                </div>
+              </div>`,
+            },
+          ],
+        },
+        {
+          to: "/showcase",
+          html: '<span class="nav-emoji">🌍</span> Showcase',
+          position: "left",
+        },
+        {
+          to: "/dashboard",
+          html: '<span class="nav-emoji">📊</span> Dashboard',
+          position: "left",
+        },
+        {
+          to: "/our-sponsors/",
+          html: '<span class="nav-emoji">💰</span> Donate',
+          position: "left",
+        },
+        {
+          type: "dropdown",
+          html: '<span class="nav-emoji">👩🏻‍💻</span> Devfolio',
+          position: "left",
+          items: [
+            {
+              label: "💻GitHub Profiles",
+              to: "https://dev.recodehive.com/devfolio",
+            },
+            {
+              label: "🎖️ GitHub Badges",
+              to: "/badges/github-badges/",
+            }, 
+          ],
+        },
+        {
+          to: "/blogs",
+          html: '<span class="nav-emoji">📰</span> Blogs',
+          position: "left",
+        },
+        {
+          type: "dropdown",
+          html: '<span class="nav-emoji">🔗</span> More',
+          position: "left",
+          items: [
+            {
+              label: "📚 E-books",
+              to: "https://learn.recodehive.com/datascience",
+            },
+            {
+              label: "🛣️ Roadmap",
+              to: "#",
+            },
+            {
+              label: "🤝 Community",
+              to: "/community",
+            },
+            {
+              label: "📺 Broadcast",
+              to: "/broadcasts/",
+            },
+            {
+              label: "🎙️ Podcast",
+              to: "/podcasts/",
+            },
+          ],
+        },
+        {
+          type: "search",
+          position: "right",
+        },
+        {
+          type: "html",
+          position: "right",
+          value: '<div id="firebase-auth-github-navbar"></div>',
+        },
+        // {
+        //   type: "dropdown",
+        //   html: '<span class="nav-emoji">🏷️</span> Tags',
+        //   position: "left",
+        //   items: [
+        //     {
+        //       label: "🏷️ Tutorial Tags 📚",
+        //       to: "/docs/tags/",
+        //       activeBaseRegex: "/docs/tags/",
+        //     },
+        //     {
+        //       label: "🏷️ Courses Tags 🎓",
+        //       to: "/courses/tags/",
+        //       activeBaseRegex: "/courses/tags/",
+        //     },
+        //     {
+        //       label: "🏷️ DSA Tags 🧠",
+        //       to: "/dsa/tags/",
+        //       activeBaseRegex: "/dsa/tags/",
+        //     },
+        //   ],
+        // },
+        
+        // {
+        //   href: "https://github.com/codeharborhub/codeharborhub",
+        //   position: "right",
+        //   className: "header-github-link",
+        //   "aria-label": "GitHub repository",
+        // },
+        // {
+        //   href: "https://www.codeharborhub.live/register",
+        //   position: "right",
+        //   className: "header-signup-link",
+        //   "aria-label": "Auth",
+        //   label: "Auth",
+        // },
+      ],
+      // hideOnScroll: true,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+    algolia: {
+      appId: "YOUR_APP_ID",
+      apiKey: "YOUR_SEARCH_API_KEY",
+      indexName: "YOUR_INDEX_NAME",
+      contextualSearch: true,
+      externalUrlRegex: "external\\.com|domain\\.com",
+      replaceSearchResultPathname: {
+        from: "/docs/",
+        to: "/",
+      },
+      searchParameters: {},
+      searchPagePath: "search",
+      insights: false,
+    },
+  } satisfies Preset.ThemeConfig,
+
+  markdown: {
+    mermaid: true,
+  },
+
+  themes: ["@docusaurus/theme-mermaid"],
+
+  plugins: [
+    [
+      "@docusaurus/plugin-ideal-image",
+      {
+        quality: 70,
+        max: 1030,
+        min: 640,
+        steps: 2,
+        disableInDev: false,
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "community",
+        path: "community",
+        routeBasePath: "community",
+        sidebarPath: require.resolve("./sidebarsCommunity.js"),
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+        showLastUpdateAuthor: true,
+        showLastUpdateTime: true,
+      },
+    ],
+  ],
+  scripts: [],
+};
+
+export default config;
